191eea539a49ccac6cdefe53816af1201cf989e2
NNTkmutnb <oleslg40102@gmail.com>
2024-10-15 16:27:10 +0700

    update

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get("/movies_reviews/:id", async (req, res) => {
     }
 });
 
-
+// get category กับ studio มาใช้ในการเพิ่มข้อมูลหนัง
 app.get("/create_movie", async (req, res) => {
     try {
         // เรียก API หรือฐานข้อมูลเพื่อดึงข้อมูลภาพยนตร์
@@ -78,8 +78,8 @@ app.post("/create_movie", async (req, res) => {
 app.get("/update_movie/:id", async (req, res) => {
     try {
         const responseMovies = await axios.get( base_url + '/movies/' + req.params.id);
-        const responseCategory = await axios.get( base_url + '/category/');
-        const responseStudio = await axios.get( base_url + '/studio/');
+        const responseCategory = await axios.get( base_url + '/category');
+        const responseStudio = await axios.get( base_url + '/studio');
         res.render("update_movie", { movie: responseMovies.data, category: responseCategory.data, studio: responseStudio.data });
     } catch (err) {
         console.error(err);
@@ -97,7 +97,7 @@ app.post("/update_movie/:id", async (req, res) => {
             flimmaking_funds: req.body.flimmaking_funds,
             movie_income: req.body.movie_income };
         await axios.put(base_url + '/movies/' + req.params.id, data);
-        res.redirect("/");
+        res.redirect(`/movies_reviews/${ req.params.id }`);
     } catch (err) {
         console.error(err);
         res.status(500).send('Error');
@@ -107,19 +107,33 @@ app.post("/update_movie/:id", async (req, res) => {
 app.get("/delete_movie/:id", async (req, res) => {
     try {
         await axios.delete(base_url + '/movies/' + req.params.id);
+        await axios.delete(base_url + '/review_movies/' + req.params.id);
         res.redirect("/");
-    } catch {
+    } catch (err) {
         console.error(err);
         res.status(500).send('Error');
     }
 });
 
 //--------------------------------------- Reviews --------------------------------------- //
-app.get("/create_review", (req, res) => {
-    res.render("create_review");
+
+
+// get ข้อมูล movie id มาใช้กับ review
+app.get("/create_review/:id", async (req, res) => {
+    try {
+        // เรียก API หรือฐานข้อมูลเพื่อดึงข้อมูลภาพยนตร์
+        const responseMovie = await axios.get(base_url + '/movies/' + req.params.id);
+        const responseReview = await axios.get(base_url + '/review/' + req.params.id);
+        
+        // ส่งข้อมูลไปยังไฟล์ EJS
+        res.render("create_review", { review: responseReview.data, movie: responseMovie.data });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
 });
 
-// เพิ่มข้อมูลหนังใหม่
+// เพิ่มข้อมูล review
 app.post("/create_review", async (req, res) => {
     try {
         const data = { movieID: req.body.movieID,
@@ -127,7 +141,7 @@ app.post("/create_review", async (req, res) => {
                 overall_score: req.body.overall_score, 
                 reviewer: req.body.reviewer };
         await axios.post(base_url + '/review', data);
-        res.redirect("/");
+        res.redirect(`/movies_reviews/${ req.body.movieID }`);
     } catch (err) {
         console.error(err);
         res.status(500).send('Error');
@@ -137,9 +151,9 @@ app.post("/create_review", async (req, res) => {
 // ดึงข้อมูลเก่ามาโชว์
 app.get("/update_review/:id", async (req, res) => {
     try {
-        const response = await axios.get(
-        base_url + '/movie_detail&reviews/' + req.params.id);
-        res.render("update_review", { reviews: response.data});
+        const response = await axios.get(base_url + '/review_id/' + req.params.id);
+        res.render("update_review", { review: response.data});
+        // console.log(response.data)
     } catch (err) {
         console.error(err);
         res.status(500).send('Error');
@@ -149,32 +163,42 @@ app.get("/update_review/:id", async (req, res) => {
 app.post("/update_review/:id", async (req, res) => {
     try {
         const data = { movieID: req.body.movieID,
+            reviewer: req.body.reviewer,
             review_detail: req.body.review_detail,
-            overall_score: req.body.overall_score, 
-            reviewer: req.body.reviewer };
+            overall_score: req.body.overall_score };
         await axios.put(base_url + '/review/' + req.params.id, data);
-        res.redirect("/");
+        res.redirect(`/movies_reviews/${ req.body.movieID}`);
     } catch (err) {
         console.error(err);
         res.status(500).send('Error');
     }
 });
 
+
 app.get("/delete_review/:id", async (req, res) => {
     try {
         await axios.delete(base_url + '/review/' + req.params.id);
-        res.redirect("/");
-    } catch {
+
+        // รับค่า movieID จาก query string
+        const movieID = req.query.movieID;
+        
+        if (movieID) {
+            res.redirect(`/movies_reviews/${movieID}`);
+        } else {
+            res.redirect(`/movies_reviews/`); // ถ้าไม่มี movieID ให้กลับไปหน้ารายการรีวิว
+        }
+    } catch (err) {
         console.error(err);
         res.status(500).send('Error');
     }
 });
 
+
 //--------------------------------------- Category --------------------------------------- //
-app.get("/category", async (req, res) => {
+app.get("/categories", async (req, res) => {
     try {
         const response = await axios.get(base_url + '/category');
-        res.render("category", { category: response.data });
+        res.render("categories", { categories: response.data });
         // console.log(response.data)
     } catch (err) {
         console.error(err);
@@ -182,6 +206,137 @@ app.get("/category", async (req, res) => {
     }
 });
 
+app.get("/create_category", async (req, res) => {
+    try {
+        // เรียก API หรือฐานข้อมูลเพื่อดึงข้อมูลภาพยนตร์
+        const responseCategory = await axios.get(base_url + '/category');
+        
+        // ส่งข้อมูลไปยังไฟล์ EJS
+        res.render("create_category", { category: responseCategory.data });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+// เพิ่มข้อมูล Category
+app.post("/create_category", async (req, res) => {
+    try {
+        const data = { name: req.body.name,
+                detail: req.body.detail };
+        await axios.post(base_url + '/category', data);
+        res.redirect("/categories");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+// ดึงข้อมูลเก่ามาโชว์
+app.get("/update_category/:id", async (req, res) => {
+    try {
+        const response = await axios.get(
+        base_url + '/category/' + req.params.id);
+        res.render("update_category", { category: response.data});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.post("/update_category/:id", async (req, res) => {
+    try {
+        const data = { name: req.body.name,
+            detail: req.body.detail };
+        await axios.put(base_url + '/category/' + req.params.id, data);
+        res.redirect("/categories");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.get("/delete_category/:id", async (req, res) => {
+    try {
+        await axios.delete(base_url + '/category/' + req.params.id);
+        res.redirect("/categories");
+    } catch {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+//--------------------------------------- Studios --------------------------------------- //
+app.get("/studios", async (req, res) => {
+    try {
+        const response = await axios.get(base_url + '/studio');
+        res.render("studios", { studios: response.data });
+        // console.log(response.data)
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.get("/create_studio", async (req, res) => {
+    try {
+        // เรียก API หรือฐานข้อมูลเพื่อดึงข้อมูลภาพยนตร์
+        const responseStudio = await axios.get(base_url + '/studio');
+        
+        // ส่งข้อมูลไปยังไฟล์ EJS
+        res.render("create_studio", { studio: responseStudio.data });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+// เพิ่มข้อมูล Category
+app.post("/create_studio", async (req, res) => {
+    try {
+        const data = { name: req.body.name,
+                detail: req.body.detail };
+        await axios.post(base_url + '/studio', data);
+        res.redirect("/studios");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+// ดึงข้อมูลเก่ามาโชว์
+app.get("/update_studio/:id", async (req, res) => {
+    try {
+        const response = await axios.get(
+        base_url + '/studio/' + req.params.id);
+        res.render("update_studio", { studio: response.data});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.post("/update_studio/:id", async (req, res) => {
+    try {
+        const data = { name: req.body.name,
+            detail: req.body.detail };
+        await axios.put(base_url + '/studio/' + req.params.id, data);
+        res.redirect("/studios");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.get("/delete_studio/:id", async (req, res) => {
+    try {
+        await axios.delete(base_url + '/studio/' + req.params.id);
+        res.redirect("/studios");
+    } catch {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
 
 app.listen(5500, () => {
     console.log('Server stated on port 5500');
