de351c93322ce10edd862ac659f9a6e135a27206
NNTkmutnb <oleslg40102@gmail.com>
2024-10-16 17:16:26 +0700

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require("path");
 const base_url = 'https://node68398-env-4961778.proen.app.ruk-com.cloud';
 
 // Set the template engine
-// app.set("views", path.join(__dirname, "/public/views"));
+app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
